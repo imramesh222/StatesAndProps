d829d66efc0d61ae255aa2aad34d9659fce91137
imramesh222 <imrameshrawat@gmail.com>
2025-02-03 20:40:59 +0545

    welcome 3

--- src/components/Welcome3.jsx
+++ src/components/Welcome3.jsx
@@ -3,10 +3,16 @@ import React from "react";
 const Welcome3 = (props) => {
   return (
     <>
-      <div className="flex justify-center bg-gradient-to-r transform transition-transform from-red-600 to-blue-700 bg-clip-text text-transparent py-4 px-2 ">
-        <h1 className="">WELCOME,</h1>
-        
-      </div>
+    <div className="flex w-96 bg-gradient-to-r from-red-600 to-blue-700 py-4 px-2 transition-transform transform hover:scale-105">
+  <h1 className="text-white font-bold text-lg">WELCOME,</h1>
+</div>
+<h1>-----------------------------------------------------------------------------------------------------------------</h1>
+     <div className="flex py-4 px-2">
+  <h1 className="bg-gradient-to-r from-red-600 to-blue-700 bg-clip-text text-transparent font-bold text-lg hover:scale-105">
+    WELCOME,
+  </h1>
+</div>
+
     </>
   );
 };
